1ae37997b1ee20d83acc396b2d7d617e981865d8
dogdolm <71942146+dogdolm@users.noreply.github.com>
2022-09-30 19:02:25 +0300

    Add files via upload
    New version

---
+++ db_generator.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+sqlite_connection = sqlite3.connect('cart.db')
+sqlite_create_table_query = '''CREATE TABLE cart (
+                                id INTEGER PRIMARY KEY,
+                                nickname TEXT NOT NULL,
+                                cart TEXT NOT NULL,
+                                passwd TEXT NOT NULL);'''
+
+cursor = sqlite_connection.cursor()
+print("База данных подключена к SQLite")
+cursor.execute(sqlite_create_table_query)
+sqlite_connection.commit()
+print("Таблица SQLite создана")
+
+cursor.close()
